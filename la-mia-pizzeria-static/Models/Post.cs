a06673b4fcfbbf492c4573d6a989b6c3a23e7ac6
SimoCrash <simone.crescenzi@hotmail.com>
2023-04-06 17:55:16 +0200

    ESERCIZIO 2: implementazione partial e section

---
+++ la-mia-pizzeria-static/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Post
+    {
+        public Post(int id, string nome, string descrizione, string foto, string prezzo)
+        {
+            Id = id;
+            Nome = nome;
+            Descrizione = descrizione;
+            Foto = foto;
+            Prezzo = prezzo;
+        }
+
+        public int Id {  get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Foto { get; set; }
+        public string Prezzo { get; set; }
+    }
+}
